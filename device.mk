5c20a059068318450bb5dc9e0be6491ca7b7c076
Aaron Kling <webgeek1234@gmail.com>
2019-01-11 16:14:24 -0600

    Add tablet characteristic and permissions
    Change-Id: I8038b08b5fb42ee3222b1d587c74b902fe5eada0

--- device.mk
+++ device.mk
@@ -1,1 +1,8 @@
+PRODUCT_CHARACTERISTICS := tablet
+
 $(call inherit-product, device/nvidia/roth/device.mk)
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
